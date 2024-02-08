5cff2cf4d3ae1c1a652bcb770544b563ee1c73de
adeearyaa <adeearyaa2013@gmail.com>
2024-02-08 19:16:14 +0800

    Set up basic pages and navigation tabs

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import "./index.css";
+import background from "./images/webbackground.png"
+import myPicture from "./images/adeepicture.png"
+import html from "./images/html.png"
+import css from "./images/css.png"
+import javascript from "./images/javascript.png"
+import typescript from "./images/typescript.png"
+import react from "./images/React.png"
+import django from "./images/django.png"
+import express from "./images/express.png"
+import node from "./images/node.png"
+import next from "./images/next.png"
+import java from "./images/java.png"
+import git from "./images/git.png"
+import webpack from "./images/webpack.png"
+import weatherApp from "./images/weatherwhisper.gif"
+import tictactoe from "./images/tictactoe.gif"
+import taskplanner from "./images/taskplanner.gif"
+import nusocial from "./images/nusocial.png"
+import github from "./images/github.svg"
+import linkedin from "./images/linkedin.png"
+import mail from "./images/mail.png"
+
+let mainContainer = document.querySelector("body");
+mainContainer.style.backgroundImage = `url(${background})`;
+
+let adeePicture = document.querySelector(".my-picture");
+adeePicture.src= myPicture
+
+let htmlIcon = document.querySelector(".html-image");
+htmlIcon.src= html
+
+let cssIcon = document.querySelector(".css-image");
+cssIcon.src= css
+
+let javascriptIcon = document.querySelector(".javascript-image");
+javascriptIcon.src= javascript
+
+let typescriptIcon = document.querySelector(".typescript-image");
+typescriptIcon.src= typescript
+
+let reactIcon = document.querySelector(".react-image");
+reactIcon.src= react
+
+let djangoIcon = document.querySelector(".django-image");
+djangoIcon.src= django
+
+let expressIcon = document.querySelector(".express-image");
+expressIcon.src= express
+
+let nodeIcon = document.querySelector(".node-image");
+nodeIcon.src= node
+
+let nextIcon = document.querySelector(".next-image");
+nextIcon.src= next
+
+let javaIcon = document.querySelector(".java-image");
+javaIcon.src= java
+
+let gitIcon = document.querySelector(".git-image");
+gitIcon.src= git
+
+let webpackIcon = document.querySelector(".webpack-image");
+webpackIcon.src= webpack
+
+let weatherAppImage = document.querySelector(".weather-page-image");
+weatherAppImage.src= weatherApp
+
+let taskPlannerImage = document.querySelector(".task-planner-image");
+taskPlannerImage.src= taskplanner
+
+let ticTacToeImage = document.querySelector(".tic-tac-toe-image");
+ticTacToeImage.src= tictactoe
+
+let nusocialImage = document.querySelector(".nus-social-image");
+nusocialImage.src= nusocial
+
+let githubImage = document.querySelector(".github-image");
+githubImage.src= github
+
+let linkedinImage = document.querySelector(".linkedin-image");
+linkedinImage.src= linkedin
+
+let mailImage = document.querySelector(".mail-image");
+mailImage.src= mail
+
+
+
+
+
